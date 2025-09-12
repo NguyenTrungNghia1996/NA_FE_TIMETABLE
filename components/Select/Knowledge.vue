cf265a393e55439ed284179bc44415d11e01c8f9
NguyenTrungNghia <nghiabgt@gmail.com>
2025-09-12 16:40:12 +0700

    update

--- components/Select/Knowledge.vue
+++ components/Select/Knowledge.vue
@@ -29,7 +29,7 @@ const loading = ref(false);
 const fetchKnowledge = async (search = "") => {
   loading.value = true;
   try {
-    const { data } = await RestApi.knowledge.list({ params: { search } });
+    const { data, error } = await RestApi.knowledge.list({ params: { search } });
 
     if (data.value?.data?.items) {
       options.value = data.value.data.items.map(item => ({
