67fbcfcc497c9aa9ee09f0c436c7a1ef1109f763
NguyenTrungNghia <nghiabgt@gmail.com>
2025-09-15 09:56:08 +0700

    update

--- components/Select/GradeLevel.vue
+++ components/Select/GradeLevel.vue
@@ -29,7 +29,7 @@ const loading = ref(false);
 const fetchGradeLevels = async (search = "") => {
   loading.value = true;
   try {
-    const { data } = await RestApi.grade_level.list({ params: { search } });
+    const { data, error } = await RestApi.grade_level.list({ params: { search } });
 
     if (data.value?.data?.items) {
       options.value = data.value.data.items.map(item => ({
