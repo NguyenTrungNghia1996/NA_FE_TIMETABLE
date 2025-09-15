7846c39e7b6ae718a020c7572ff72b5da5493a46
NguyenTrungNghia <nghiabgt@gmail.com>
2025-09-15 16:13:13 +0700

    update

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="p-4">
-    <a-button type="primary" @click="visible = true">Đăng ký dùng thử</a-button>
-    <a-modal
-      v-model:open="visible"
-      title="Đăng ký tài khoản dùng thử"
-      @cancel="visible = false"
-      :width="800"
-      :footer="null"
-    >
-      <RegisterForm @success="visible = false" />
-    </a-modal>
-  </div>
+  <div class="p-4"></div>
 </template>
 
-<script setup>
-const visible = ref(false)
-</script>
+<script setup></script>
